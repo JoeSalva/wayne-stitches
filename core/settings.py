c75c13ba5bd07db31d8dfcb2fde3343b6477a62a
JoeSalva <salvagainchibu@gmail.com>
2025-08-01 10:23:19 +0100

    Modified settings.py

--- core/settings.py
+++ core/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os.path
-# import os
+import os
 from pathlib import Path
 from tempfile import template
 
