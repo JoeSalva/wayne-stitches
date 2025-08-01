cae9c485561d2ae859e5bf085a086826fb306f1b
JoeSalva <salvagainchibu@gmail.com>
2025-08-01 10:21:19 +0100

    Modified settings.py

--- core/settings.py
+++ core/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "fallback-dev-secret")
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 # DEBUG  = "True"
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "").split(" ")
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "").split()
+print("ALLOWED_HOSTS:", ALLOWED_HOSTS)
 # ALLOWED_HOSTS = ['*']
 
 # Application definition
